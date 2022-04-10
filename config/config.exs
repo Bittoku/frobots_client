1d49f56aeb7bf83ef123b6ab1b4cfdd7f85aef01
digitsu <digitsu@gmail.com>
2022-04-10 15:18:44 +0900

    remove the web server part from the client

--- config/config.exs
+++ config/config.exs
@@ -9,26 +9,6 @@
 # move said applications out of the umbrella.
 import Config
 
-config :frobots_web,
-  generators: [context_app: :frobots]
-
-# Configures the endpoint
-config :frobots_web, FrobotsWeb.Endpoint,
-  url: [host: "localhost"],
-  render_errors: [view: FrobotsWeb.ErrorView, accepts: ~w(json), layout: false],
-  pubsub_server: Frobots.PubSub,
-  live_view: [signing_salt: "iauRoAMU"]
-
-# Configure esbuild (the version is required)
-config :esbuild,
-  version: "0.14.0",
-  default: [
-    args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
-    cd: Path.expand("../apps/frobots_web/assets", __DIR__),
-    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
-  ]
-
 # Configure the main viewport for the Scenic application
 config :frobots_scenic, :viewport, %{
   name: :main_viewport,
@@ -48,8 +28,6 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
-config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
